45a6a83ec36ce06a939f29bb2c7e605808661ba6
alextk37 <alextk37@yandex.ru>
2024-06-13 00:58:55 +0300

    add config file for keywords search

---
+++ hh_db/get_data/company_config.py
@@ -0,0 +1,12 @@
+company_key = [
+               'ООО Мад Софт',
+               'Smedia Buying Holding',
+               'Тензор',
+               'ООО АйТи-Солюшн',
+               'Just AI',
+               'Формат кода',
+               'Группа компаний eLama',
+               'Доктор Веб',
+               'JetLend',
+               'AI Hub Group',
+               ]
